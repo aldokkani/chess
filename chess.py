19230e08288a6fbe8e2ceeed4c89fee55619fa6f
Hossam Al-Dokkani <hossam.o@trufla.com>
2018-11-15 21:16:32 +0200

    Create movement rules for all the pieces

--- chess.py
+++ chess.py
@@ -1,11 +1,6 @@
 import pygame
 import math
-
-
-class Piece(object):
-    def __init__(self, type_, color):
-        self.type = type_
-        self.color = (0, 255, 0) if color else (0, 0, 255)
+from pieces import *
 
 
 class Chess(object):
@@ -36,7 +31,7 @@ class Chess(object):
     def draw_board(self):
         x, y = 0, 0
         for i in range(8 * 8):
-            self.draw_square(x, y)
+            self.draw_square((x, y))
             x += 1
             if (i + 1) % 8 == 0:
                 y += 1
@@ -57,56 +52,58 @@ class Chess(object):
     def move_piece(self, pos):
         x = math.floor(pos[0] / 100 % 10)
         y = math.floor(pos[1] / 100 % 10)
-        print(x, y, pos)
+        print(x, y, x+y)
 
         if (x, y) in self.pieces_on_board:
             self.piece_to_move = (x, y)
         elif self.piece_to_move:
-            piece = self.pieces_on_board.pop(self.piece_to_move)
+            piece = self.pieces_on_board[self.piece_to_move]
+            if piece.can_move(self.piece_to_move, (x, y)):
+                self.draw_piece(piece, (x, y))
+                self.pieces_on_board.update({(x, y): piece})
+                # Clear the old square
+                self.draw_square(self.piece_to_move)
+                self.pieces_on_board.pop(self.piece_to_move)
 
-            self.draw_piece(piece, x, y)
-            self.pieces_on_board.update({(x, y): piece})
-
-            # Clear the old square
-            self.draw_square(*self.piece_to_move)
             self.piece_to_move = None
 
-    def draw_square(self, x, y):
-        if y % 2:
-            color = self.colors['white'] if x % 2 else self.colors['black']
+    def draw_square(self, pos):
+        if pos[1] % 2:
+            color = self.colors['white'] if pos[0] % 2 else self.colors['black']
         else:
-            color = self.colors['black'] if x % 2 else self.colors['white']
+            color = self.colors['black'] if pos[0] % 2 else self.colors['white']
 
         cell = pygame.Surface((100, 100))
 
         cellpos = cell.get_rect()
-        cellpos.x = x * 100
-        cellpos.y = y * 100
+        cellpos.x = pos[0] * 100
+        cellpos.y = pos[1] * 100
 
         cell.fill(color)
         cell = cell.convert()
         self.board.blit(cell, cellpos)
 
-    def draw_piece(self, piece, x, y):
+    def draw_piece(self, piece, pos):
         font = pygame.font.Font(None, 30)
         text = font.render(piece.type, 1, piece.color)
         textpos = text.get_rect()
-        textpos.centerx = x * 100 + 50
-        textpos.centery = y * 100 + 50
+        textpos.centerx = pos[0] * 100 + 50
+        textpos.centery = pos[1] * 100 + 50
         self.board.blit(text, textpos)
 
     def init_setup(self):
-        pieces = "RNBQKBNR"
-        for i, p in enumerate(pieces):
-            black_piece = Piece(p, False)
-            black_pawn = Piece('P', False)
-            white_piece = Piece(p, True)
-            white_pawn = Piece('P', True)
+        pieces = ['Rock', 'Knight', 'Bishop', 'Queen', 'King', 'Bishop', 'Knight', 'Rock']
 
-            self.draw_piece(black_piece, i, 0)
-            self.draw_piece(black_pawn, i, 1)
-            self.draw_piece(white_pawn, i, 6)
-            self.draw_piece(white_piece, i, 7)
+        for i, p in enumerate(pieces):
+            black_piece = PieceFactory.get_piece(p, False)
+            black_pawn = PieceFactory.get_piece('Pawn', False)
+            white_piece = PieceFactory.get_piece(p, True)
+            white_pawn = PieceFactory.get_piece('Pawn', True)
+
+            self.draw_piece(black_piece, (i, 0))
+            self.draw_piece(black_pawn, (i, 1))
+            self.draw_piece(white_pawn, (i, 6))
+            self.draw_piece(white_piece, (i, 7))
 
             self.pieces_on_board.update({(i, 0): black_piece})
             self.pieces_on_board.update({(i, 1): black_pawn})
---
+++ pieces.py
@@ -0,0 +1,67 @@
+import sys
+
+
+class Piece(object):
+    def __init__(self, type_, color):
+        self.type = type_
+        # If color == True then it's a white piece else it's a black piece.
+        self.color = (0, 255, 0) if color else (0, 0, 255)
+
+
+class Pawn(Piece):
+    moved = False
+
+    def can_move(self, current_pos, new_pos):
+        good_white = current_pos[0] == new_pos[0] and self.color == (0, 255, 0)
+        good_black = current_pos[0] == new_pos[0] and self.color == (0, 0, 255)
+
+        truthy = list([good_white and current_pos[1] == new_pos[1] + 1])
+        truthy.append(good_white and not self.moved and current_pos[1] == new_pos[1] + 2)
+        truthy.append(good_black and current_pos[1] + 1 == new_pos[1])
+        truthy.append(good_black and not self.moved and current_pos[1] + 2 == new_pos[1])
+
+        if any(truthy):
+            self.moved = True
+            return True
+        return False
+
+
+class Knight(Piece):
+    @staticmethod
+    def can_move(current_pos, new_pos):
+        abs_x = abs(current_pos[0] - new_pos[0])
+        abs_y = abs(current_pos[1] - new_pos[1])
+        return abs_x == 1 and abs_y == 2 or abs_x == 2 and abs_y == 1
+
+
+class Bishop(Piece):
+    @staticmethod
+    def can_move(current_pos, new_pos):
+        return abs(current_pos[0] - new_pos[0]) == abs(current_pos[1] - new_pos[1])
+
+
+class Rock(Piece):
+    @staticmethod
+    def can_move(current_pos, new_pos):
+        return current_pos[0] == new_pos[0] or current_pos[1] == new_pos[1]
+
+
+class Queen(Piece):
+    @staticmethod
+    def can_move(current_pos, new_pos):
+        truthy = list([current_pos[0] == new_pos[0]])
+        truthy.append(current_pos[1] == new_pos[1])
+        truthy.append(abs(current_pos[0] - new_pos[0]) == abs(current_pos[1] - new_pos[1]))
+        return any(truthy)
+
+
+class King(Piece):
+    @staticmethod
+    def can_move(current_pos, new_pos):
+        return abs(current_pos[0] - new_pos[0]) in [0, 1] and abs(current_pos[1] - new_pos[1]) in [0, 1]
+
+
+class PieceFactory(object):
+    @staticmethod
+    def get_piece(type_, color):
+        return getattr(sys.modules[__name__], type_)(type_, color)
